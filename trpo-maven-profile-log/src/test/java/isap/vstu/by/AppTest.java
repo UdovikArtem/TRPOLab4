5f115b319aed86e7dce087cd407498bb2a779aab
ArtemEretik <artem.ud2003@gmail.com>
2022-05-10 15:09:28 +0200

    createdprogect

---
+++ trpo-maven-profile-log/src/test/java/isap/vstu/by/AppTest.java
@@ -0,0 +1,20 @@
+package isap.vstu.by;
+
+import static org.junit.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
